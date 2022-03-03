29e0b0d61582965d3f0e904ac8feae35130cbcf6
zcKO <studyJcHub@gmail.com>
2022-03-03 16:20:22 +0900

    binding 변수를 이용하여, 이벤트 처리 진행  - 실제 API 서버 연동 예정

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
@@ -13,5 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+        binding.btnLogin.setOnClickListener {
+
+            val inputId = binding.edtId.text.toString()
+            val inputPw = binding.edtPassword.text.toString()
+
+            // API 서버에 아이디 / 비번을 보내서 실제로 회원인지 검사 => 로그인 시도.
+
+        }
+
+    }
+
+    fun setValues() {
+
+    }
+
 }
