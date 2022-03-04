3dc12e3aa644b60ca08eca83378fcf1c9f77e8e3
zcKO <studyJcHub@gmail.com>
2022-03-04 14:51:18 +0900

    서버 통신이 완료된 후의 처리를, UI 단에서 코딩  - Toast 실행 시 앱이 죽는다.

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
@@ -2,9 +2,11 @@ package com.jc.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.jc.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 import com.jc.apipractice_okhttp_20220303.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,7 +28,21 @@ class MainActivity : AppCompatActivity() {
             val inputPw = binding.edtPassword.text.toString()
 
             // API 서버에 아이디 / 비번을 보내서 실제로 회원인지 검사 => 로그인 시도.
-            ServerUtil.postRequestLogin(inputId, inputPw)
+            ServerUtil.postRequestLogin(inputId, inputPw, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObject: JSONObject) {
+
+                    // 화면의 입장에서, 로그인 결과를 받아서 처리할 코드
+                    val code = jsonObject.getInt("code")
+
+                    if (code == 200) {
+
+                    } else {
+                        Toast.makeText(this@MainActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+            })
 
         }
 
