3fcb522b539bc19d8c8963e98989eee60e821c2a
zcKO <studyJcHub@gmail.com>
2022-03-04 12:19:32 +0900

    jsonObj > dataObj > userObj > 닉네임 추출

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -73,6 +73,12 @@ class ServerUtil {
 
                     if (code == 200) {
                         Log.d("로그인 시도", "성공!")
+
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val userObj = dataObj.getJSONObject("user")
+                        val nickname = userObj.getString("nick_name")
+                        Log.d("로그인 한 사람 유저 닉네임", nickname)
+
                     } else {
                         Log.d("로그인 시도", "실패.")
 
