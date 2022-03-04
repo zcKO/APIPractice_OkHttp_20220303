de5e4f07bc416e86138b95815450b4f54ea48f23
zcKO <studyJcHub@gmail.com>
2022-03-04 14:56:26 +0900

    runOnUiThread 로, UI 처리는 메인스레드에서.

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
@@ -32,12 +32,22 @@ class MainActivity : AppCompatActivity() {
                 override fun onResponse(jsonObject: JSONObject) {
 
                     // 화면의 입장에서, 로그인 결과를 받아서 처리할 코드
+                    // 서버에 다녀오고 실행 : 라이브러리가 자동으로 백그라운드에서 돌도록 만들어 둔 코드
                     val code = jsonObject.getInt("code")
 
                     if (code == 200) {
 
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        }
+
                     } else {
-                        Toast.makeText(this@MainActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        // Toast : UI 조작 하는 코드 => 백그라운드에서 UI 를 건드리면, 위험한 동작으로 간주하고 앱을 강제 종료한다.
+                        runOnUiThread {
+                            // Toast 를 띄우는 코드만, UI 전담 스레드에서 실행하도록 한다.
+                            Toast.makeText(this@MainActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        }
+
                     }
 
                 }
