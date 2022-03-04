8885c6f5e589d70affff0a41614c93f0c1602d47
zcKO <studyJcHub@gmail.com>
2022-03-04 10:04:20 +0900

    로그인 기능 수행 함수 제작 중  - 별도 클래스 생성 (ServerUtil)  - companion object 로 static 과 유사한 효과  - 로그인 담당 함수 -> url 정리

---
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -0,0 +1,25 @@
+package com.jc.apipractice_okhttp_20220303.utils
+
+class ServerUtil {
+
+    // 서버에 Request 를 보내는 역할
+    // 함수를 만들 때, 어떤 객체가 실행해도 결과만 잘 나오면 그만인 함수.
+    // Kotlin 에서 static 에 해당하는 개념 -> companion object {  }
+
+    companion object {
+
+        // 서버 컴퓨터 주소만 변수로 저장 (관리 일원화) -> 외부 노출은 안되게 한다.
+        private const val BASE_URL = "http://54.180.52.26"
+
+        // 로그인 기능 호출 함수
+        fun postRequestLogin(id: String, pw: String) {
+            // request 제작 -> 실제 호출 -> 서버의 응답을 화면에 전달
+
+            // 제작 1) 어느 주소 (url) 로 접근할지 지정 -> 서버 주소 + 기능 주소
+            val urlString = "${BASE_URL}/user"
+
+        }
+
+    }
+
+}
