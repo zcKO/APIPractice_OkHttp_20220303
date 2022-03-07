1a632820607f30dab554962df363c351952db400
zcKO <studyjchub@gmail.com>
2022-03-07 09:42:40 +0900

    MainActivity 이름 변경 > LoginActivity

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/LoginActivity.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/LoginActivity.kt
@@ -4,17 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.jc.apipractice_okhttp_20220303.databinding.ActivityMainBinding
+import com.jc.apipractice_okhttp_20220303.databinding.ActivityLoginBinding
 import com.jc.apipractice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         setupEvents()
         setValues()
