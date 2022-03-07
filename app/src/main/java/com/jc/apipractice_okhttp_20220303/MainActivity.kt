2229a8fbb8dfd017877d5196b92afd36a9c91e11
zcKO <studyjchub@gmail.com>
2022-03-07 15:47:22 +0900

    연습 문제 마무리  - 토큰을 첨부해서, 토근에 맞는 내 정보를 받아오기

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
@@ -3,6 +3,8 @@ package com.jc.apipractice_okhttp_20220303
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.jc.apipractice_okhttp_20220303.databinding.ActivityMainBinding
+import com.jc.apipractice_okhttp_20220303.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -23,6 +25,22 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
 
         // 화면의 TextView 에 닉네임을 보여주기 위한 작업
+        ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(jsonObject: JSONObject) {
+
+                val dataObj = jsonObject.getJSONObject("data")
+                val userObj = dataObj.getJSONObject("user")
+                val nickname = userObj.getString("nick_name")
+
+                runOnUiThread {
+
+                    binding.txtLoginUserNickname.text = nickname
+
+                }
+
+            }
+
+        })
 
     }
 
