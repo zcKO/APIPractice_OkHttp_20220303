bc5c5b97f1aad0ab2094984d5b0487b1cd1fc1b6
zcKO <studyjchub@gmail.com>
2022-03-07 17:04:16 +0900

    JSONArray 의 JSONObject 들을 하나씩 변수에 (반복으로) 담아서 로그로 출력

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.jc.apipractice_okhttp_20220303
 
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.jc.apipractice_okhttp_20220303.data.TopicData
 import com.jc.apipractice_okhttp_20220303.databinding.ActivityMainBinding
@@ -40,10 +41,18 @@ class MainActivity : BaseActivity() {
 
                 // 서버가 주는 토론 주제 목록 파싱 -> mTopicList 에 추가하기.
                 val dataObj = jsonObject.getJSONObject("data")
-                val topics = dataObj.getJSONArray("topics")
+                val topicsArr = dataObj.getJSONArray("topics")
 
                 // topicsArr 내부를 하나씩 추출 (JOSONObject {  }) => TopicData() 로 변환.
+                // JSONObject 는 for-each 를 지원하지 않는다. (차후 : ArrayList 로 변환 후 for-each 활용 예정 - retrofit)
+                // Java : for (int i = 0; i < "배열".length(); i++) 와 완전히 동일한 문법.
+                for (i in 0 until topicsArr.length()) {
 
+                    // [   ] -> { }, { }, { }, ..... 순서에 맞는 {} 를 변수에 담는다.
+                    // JSON 파싱의 { } => (JSONArray 에게서) JSONObject 로 추출
+                    val topicObj = topicsArr.getJSONObject(i)
+                    Log.d("받아낸 주제", topicObj.toString())
+                }
 
             }
 
