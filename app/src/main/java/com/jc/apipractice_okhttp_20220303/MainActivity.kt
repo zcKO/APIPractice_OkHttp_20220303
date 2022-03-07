2accbd4d3a4994b9a40a09a492e25e8398d32a46
zcKO <studyjchub@gmail.com>
2022-03-07 16:55:26 +0900

    topics 라는 JSONArray 추출  - 내부를 분석할 예정

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jc.apipractice_okhttp_20220303
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.jc.apipractice_okhttp_20220303.data.TopicData
 import com.jc.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 import com.jc.apipractice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
@@ -9,6 +10,9 @@ import org.json.JSONObject
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    // 실제로 서버가 내려주는 주제 목록을 담을 그릇
+    val mTopicList = ArrayList<TopicData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,19 +28,22 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
-        // 화면의 TextView 에 닉네임을 보여주기 위한 작업
-        ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler {
+        // 메인 화면 정보 가져오기 => API 호출 / 응답 처리
+        getTopicListFromServer()
+
+    }
+
+    private fun getTopicListFromServer() {
+
+        ServerUtil.getRequestMainInfo(mContext, object: ServerUtil.JsonResponseHandler {
             override fun onResponse(jsonObject: JSONObject) {
 
+                // 서버가 주는 토론 주제 목록 파싱 -> mTopicList 에 추가하기.
                 val dataObj = jsonObject.getJSONObject("data")
-                val userObj = dataObj.getJSONObject("user")
-                val nickname = userObj.getString("nick_name")
+                val topics = dataObj.getJSONArray("topics")
 
-                runOnUiThread {
+                // topicsArr 내부를 하나씩 추출 (JOSONObject {  }) => TopicData() 로 변환.
 
-
-
-                }
 
             }
 
