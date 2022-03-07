04c4baf507ad1dc66f9be850db9cc143f4e64653
zcKO <studyjchub@gmail.com>
2022-03-07 09:45:55 +0900

    메인화면 추가, 데이터 바인딩 세팅

---
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.jc.apipractice_okhttp_20220303
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.jc.apipractice_okhttp_20220303.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
