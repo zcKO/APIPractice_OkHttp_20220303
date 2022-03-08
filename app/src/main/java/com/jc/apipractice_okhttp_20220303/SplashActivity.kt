b79ffbd7e4663e7917793901c2dd96f587ba416d
zcKO <studyjchub@gmail.com>
2022-03-08 09:47:39 +0900

    로딩화면 추가

---
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.jc.apipractice_okhttp_20220303
+
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
