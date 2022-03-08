7342c9fed5d46eb546ad2906c9944583bb832b23
zcKO <studyjchub@gmail.com>
2022-03-08 12:06:33 +0900

    선택 진영 표현 클래스 추가

---
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/data/SideData.kt
@@ -0,0 +1,25 @@
+package com.jc.apipractice_okhttp_20220303.data
+
+import org.json.JSONObject
+
+// 토론의 선택 진영 (이름, id 값 등등)을 표현
+class SideData {
+    var id = 0
+    var title = ""
+    var voteCount = 0
+
+    companion object {
+
+        fun getSideDataFromJson(jsonObj: JSONObject): SideData {
+
+            return SideData().apply {
+                id = jsonObj.getInt("id")
+                title = jsonObj.getString("title")
+                voteCount = jsonObj.getInt("vote_count")
+            }
+
+        }
+
+    }
+
+}
