f80936a4249db46511652a14525a49e64dfb52e9
zcKO <studyjchub@gmail.com>
2022-03-08 10:49:51 +0900

    진ㅇ닙한 화면의 기본 UI / 데이터 세팅

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
@@ -2,6 +2,7 @@ package com.jc.apipractice_okhttp_20220303
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.jc.apipractice_okhttp_20220303.data.TopicData
 import com.jc.apipractice_okhttp_20220303.databinding.ActivityViewTopicDetailBinding
 
@@ -27,6 +28,11 @@ class ViewTopicDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        binding.txtTitle.text = mTopicData.title
+        Glide.with(mContext)
+            .load(mTopicData.imageURL)
+            .into(binding.imgTopicBackground)
 
     }
 
