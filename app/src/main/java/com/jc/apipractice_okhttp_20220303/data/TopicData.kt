5a5dc7d2fafa7b78c0b53b5541d86475374af79b
zcKO <studyjchub@gmail.com>
2022-03-08 12:12:23 +0900

    토론 주제의 멤버 변수로, 선택 진영 목록 공간 추가  - 토론 주제 파싱 시에, 하위 선택 진영 목록도 파싱해서 > ArrayList 에 추가

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/data/TopicData.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/data/TopicData.kt
@@ -10,6 +10,9 @@ class TopicData : Serializable {
     var imageURL = ""       // 서버 : img_url, 앱 : imageURL 변수명이 다른 경우
     var replyCount = 0
 
+    // 하나의 토론주제 : 여러 개의 (목록) 선택 진영이 필요하다.
+    val sideList = ArrayList<SideData>()
+
     companion object {
         // 토론 주제 정보를 담고 있는 JSONObject 가 들어오면 > TopicData 형태로 변환해주는 함수. => static 메서드
         fun getTopicDataFromJson(jsonObj: JSONObject): TopicData {
@@ -35,7 +38,10 @@ class TopicData : Serializable {
                 val sideObj = sidesArr.getJSONObject(i)
 
                 // sideObj 도, SideData 로 (선택 진영)
+                val sideData = SideData.getSideDataFromJson(jsonObj)
 
+                // topicData 변수의 하위 목록으로 등록
+                topicData.sideList.add(sideData)
             }
 
 
