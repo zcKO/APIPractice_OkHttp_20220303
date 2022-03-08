b86e7885df121e787a7d9e9ce4c1bb9697ea1510
zcKO <studyjchub@gmail.com>
2022-03-08 16:52:06 +0900

    UserData 클래스 추가 + ReplyData 파싱에 활용  - ListView 에 사용할 데이터 클래스 준비 작업

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/data/ReplyData.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/data/ReplyData.kt
@@ -7,6 +7,9 @@ class ReplyData(
     var content: String
 ) {
 
+    var writer = UserData()     // 모든 댓글에는 작성자가 있다. null 가능성이 없다.
+
+
     // 보조 생성자 추가 연습 : 파라미터 없는 생성자.
     constructor() : this(0, "내용 없음")
 
@@ -17,6 +20,9 @@ class ReplyData(
             val replyData = ReplyData()
 
             // JSON 정보 > 멤버 변수 채우기
+            replyData.id = jsonObj.getInt("id")
+            replyData.content = jsonObj.getString("content")
+            replyData.writer = UserData.getUserDataFromJson(jsonObj.getJSONObject("user"))
 
             return replyData
 
