cd8387a8f18bc1c27bd761c52a2c1226d0380b40
zcKO <studyjchub@gmail.com>
2022-03-08 10:02:33 +0900

    내 정보 가져오기를 시도 => 그 결과에 따라 토큰 유효 여부 검증

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/SplashActivity.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/SplashActivity.kt
@@ -3,6 +3,9 @@ package com.jc.apipractice_okhttp_20220303
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.jc.apipractice_okhttp_20220303.utils.ContextUtil
+import com.jc.apipractice_okhttp_20220303.utils.ServerUtil
+import org.json.JSONObject
 
 class SplashActivity : BaseActivity() {
 
@@ -29,13 +32,31 @@ class SplashActivity : BaseActivity() {
 
             // 자동 로그인을 해도 되는지 질문
             // 1. 사용자가 자동로그인 의사를 OK 했는지 확인
+            val userAutoLogin = ContextUtil.getAutoLogin(mContext)
             // 2. 로그인 시에 받아낸 토큰 값이 지금도 유효한지 검증
-            if () {
-                // 둘다 OK 라면 메인화면으로 이동
+            var isTokenOk = false       // 검사를 통과하면 true 로 변경 예정
+            // 2-1. 저장된 토큰이 있는지 확인      (임시 방편)
+            // 2-2. 그 토큰이 사용자 정보를 불러내는지 확인  (실제 방법) 여기서 사용할 방법
+            ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObject: JSONObject) {
 
-            } else {
-                // 아니라면, 로그인 화면으로 이동
-            }
+                    // 내정보를 잘 가져왔다면, code 값을 200으로 내려준다.
+                    val code = jsonObject.getInt("code")
+
+                    // 200 이 아니라면, 무슨 이유던 사용자 정보 로드 실패 (토큰이 유효하지 않다.)
+                    isTokenOk = (code == 200)
+
+                }
+
+            })
+
+
+//            if () {
+//                // 둘다 OK 라면 메인화면으로 이동
+//
+//            } else {
+//                // 아니라면, 로그인 화면으로 이동
+//            }
 
         }, 2500)
 
