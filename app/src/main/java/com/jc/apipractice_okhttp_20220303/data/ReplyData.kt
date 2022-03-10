be1eb6e618b964862074a941cf7ce6d80fab2851
zcKO <studyjchub@gmail.com>
2022-03-10 10:55:24 +0900

    실제로 서버가 주는 댓글 작성 일시 파싱

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/data/ReplyData.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/data/ReplyData.kt
@@ -1,6 +1,7 @@
 package com.jc.apipractice_okhttp_20220303.data
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ReplyData(
@@ -35,10 +36,22 @@ class ReplyData(
             // Calendar 로 되어있는 작성일시의 시간을, 서버가 알려주는 댓글 작성 일시로 맞춰줘야 한다.
 
             // 임시 1) 2022-01-12 10:55:35 로 변경 (한번에 모두 변경)
-            replyData.createdAt.set(2022, Calendar.JANUARY, 12, 10, 55, 35)
-
+//            replyData.createdAt.set(2022, Calendar.JANUARY, 12, 10, 55, 35)
             // 임시 2) 연도만 2021 년으로 변경 (항목을 찍어서 변경)
-            replyData.createdAt.set(Calendar.YEAR, 2021)
+//            replyData.createdAt.set(Calendar.YEAR, 2021)
+
+            // 실제) 서버가 주는 created_at 에 담긴 String 을 => parse 해서, Calendar 로 변경
+            // createAt 변수의 "일/시" 값으로 parse 결과물 사용.
+
+            // 서버가 주는 양식을 보고, 그대로 찍는다.
+            val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+            // create_at 으로 내려오는 문구 (서버가 주는 String 임시 저장)
+            val createdAtStr = jsonObj.getString("created_at")
+
+            // createdAtStr 변수를 Date 로 변경 (parse) 의 결과물을 Calendar 의 time 에 대입한다.
+            replyData.createdAt.time = sdf.parse(createdAtStr)
+
 
             return replyData
 
