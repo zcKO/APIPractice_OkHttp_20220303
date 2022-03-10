5b5520e43379c4e1fb3100c0a62732cfc88989ef
zcKO <studyjchub@gmail.com>
2022-03-10 10:08:13 +0900

    임시 연습 - 작성 일시의 항목들을 하나씩 꺼내서 일자 형태로 가공  - 월 값은 0 ~ 11 이므로, 1작게 나온다. (보정이 필요)

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/adapters/ReplyAdapter.kt
@@ -11,6 +11,7 @@ import com.bumptech.glide.Glide
 import com.jc.apipractice_okhttp_20220303.R
 import com.jc.apipractice_okhttp_20220303.data.ReplyData
 import com.jc.apipractice_okhttp_20220303.data.TopicData
+import java.util.*
 
 class ReplyAdapter(
     val mContext: Context,
@@ -32,12 +33,15 @@ class ReplyAdapter(
         val txtSelectorSide = row.findViewById<TextView>(R.id.txtSelectedSide)
         val txtWriterNickname = row.findViewById<TextView>(R.id.txtWriterNickname)
         val txtReplyContent = row.findViewById<TextView>(R.id.txtReplyContent)
-
+        val txtCreatedAt = row.findViewById<TextView>(R.id.txtCreatedAT)
 
         txtReplyContent.text =  data.content
         txtWriterNickname.text = data.writer.nickname
         txtSelectorSide.text = "[${data.selectedSide.title}]"
 
+        // 임시 - 작성 일자만 "2022-03-10" 형태로 표현 => "연 / 월 / 일" 데이터로 가공
+        // 월은 1 작게 나온다. +1 로 보정
+        txtCreatedAt.text = "${data.createdAt.get(Calendar.YEAR)}-${data.createdAt.get(Calendar.MONTH) + 1}-${data.createdAt.get(Calendar.DAY_OF_MONTH)}"
 
         return row
 
