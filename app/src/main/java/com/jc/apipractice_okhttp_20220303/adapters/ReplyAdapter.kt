0c1ae16cceef294e77eea51990386ef158f09d41
zcKO <studyjchub@gmail.com>
2022-03-11 12:16:05 +0900

    손으로 누르고 있을 때, 좋아요 배경 어둡게 처리  - selector 태그 활용법

--- app/src/main/java/com/jc/apipractice_okhttp_20220303/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/jc/apipractice_okhttp_20220303/adapters/ReplyAdapter.kt
@@ -139,7 +139,7 @@ class ReplyAdapter(
         // 좋아요가 눌렸는지, 아닌지. 글씨 색상 변경 / 배경 drawable 도 설정
         if (data.isMyLike) {
             txtLikeCount.setTextColor(ContextCompat.getColor(mContext, R.color.naver_red))
-            txtLikeCount.setBackgroundResource(R.drawable.naver_red_border)
+            txtLikeCount.setBackgroundResource(R.drawable.naver_red_border_box)
         } else {
             txtLikeCount.setTextColor(ContextCompat.getColor(mContext, R.color.deep_dark_gray))
             txtLikeCount.setBackgroundResource(R.drawable.dark_gray_border_box)
